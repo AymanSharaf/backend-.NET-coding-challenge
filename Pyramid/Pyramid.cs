c13f23d0fe25e7fe25badb52860fa6dfc88d9d1f
AymanAbdelaal <ayman.s.ismaeel@gmail.com>
2023-01-15 01:01:10 +0100

    Added Pyramid code

--- Pyramid/Pyramid.cs
+++ Pyramid/Pyramid.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 /*
  
@@ -18,9 +19,43 @@ namespace Pyramid
     {
         private static void Pyramid(int height)
         {
-            Console.WriteLine("A beautiful pyramid");
+            StringBuilder pyramid = new StringBuilder();
+
+            for (int i = 0; i < height; i++)
+            {
+                var line = BuildLine(i, height);
+                pyramid.AppendLine(line);
+            }
+
+            Console.WriteLine(pyramid);
         }
-        
+
+        private static string BuildLine(int lineIndex, int pyramidHeight)
+        {
+            var line = new StringBuilder();
+
+            AddSpaces(line, lineIndex, pyramidHeight);
+            AddStars(line, lineIndex);
+
+            return line.ToString();
+        }
+
+        private static void AddStars(StringBuilder line, int lineIndex)
+        {
+            for (int j = 0; j < (lineIndex * 2) + 1; j++)
+            {
+                line.Append("*");
+            }
+        }
+
+        private static void AddSpaces(StringBuilder line, int lineIndex, int pyramidHeight)
+        {
+            for (int i = 0; i < pyramidHeight - lineIndex - 1; i++)
+            {
+                line.Append(" ");
+            }
+        }
+
         public static void Main(string[] args)
         {
             Pyramid(5);
